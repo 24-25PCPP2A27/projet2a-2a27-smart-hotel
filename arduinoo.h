fe9e731fe23363396b4ba14189aa4bf6f507a0d4
maram <maram.balti@esprit.tn>
2024-12-07 02:08:56 +0100

    first commit

---
+++ arduinoo.h
@@ -0,0 +1,21 @@
+#ifndef ARDUINOO_H
+#define ARDUINOO_H
+
+
+#include <QSerialPort>
+
+class Arduino
+{
+public:
+    Arduino();
+    ~Arduino();
+
+    QSerialPort* getserial();      // Retourne l'objet QSerialPort
+    QByteArray read_from_arduino(); // Lit les données depuis Arduino
+    void write_to_arduino(const QByteArray &data); // Écrit des données vers Arduino
+
+private:
+    QSerialPort *serial;  // Pointeur vers l'objet QSerialPort
+};
+
+#endif // ARDUINOO_H
